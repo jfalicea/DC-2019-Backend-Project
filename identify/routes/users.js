9cc0c727aad72f54a914e31c3ddad3de649d5adf
Eugene Kim <ekim1707@gmail.com>
2019-08-22 10:02:02 -0400

    second push

--- identify/routes/users.js
+++ identify/routes/users.js
@@ -2,79 +2,71 @@ var express = require('express');
 var router = express.Router();
 const { sanitizeBody } = require('express-validator');
 const User = require('../models/User');
-
+const Swal = require('sweetalert2');
 const expressSession = require('express-session');
 
 const sessionOptions = {
   // your 'secret' string should be on 'gitignore' (for example your .env file) so other people cannot see it on github
   secret: 'sda;lkjsd',
   resave: false,
-  saveUninitialized: false
+  saveUninitialized: false,
+  // cookie: { maxAge: 60000 }
 };
 
 router.use(expressSession(sessionOptions));
+
+// router.all('*', (req, res, next) => {
+//   if((!req.session.email) && (req.url !== '/login')){
+//     res.redirect('/login')
+//   }else{
+//     console.log(req.session.email);
+//     console.log(User.getAll(1).email);
+//     next();
+//   }
+// });
+
 /* GET users listing. */
 router.get('/', function(req, res) {
   res.send('hello');
 });
 
-router.get('/register', function(req, res) {
-  res.render('register', {
-    title: 'Register'
-  });
-});
-
 router.post('/registerProcess', [
-  sanitizeBody('username').escape(),
-  sanitizeBody('displayname').escape(),
+  sanitizeBody('first_name').escape(),
+  sanitizeBody('last_name').escape(),
+  sanitizeBody('email').escape(),
+  sanitizeBody('company_name').escape(),
   sanitizeBody('password').escape(),
 ], async (req, res) => {
   console.log(req.body);
  const newUserInfo = await User.createUser(req.body);
  console.log(newUserInfo);
-res.json(newUserInfo);
+  res.render('reg-success');
 });
 
-router.get('/loginProcess', function(req, res) {
-  res.render('login', {
-    title: 'Login'
+router.get('/database', async (req, res) => {
+  const userData = await User.getUsers();
+  const parsedData = JSON.stringify(userData);
+  res.render('database', {
+    title: 'Database',
+    data: parsedData
   });
 });
 
 router.post('/loginProcess', [
-  sanitizeBody('username').escape(),
+  sanitizeBody('email').escape(),
   sanitizeBody('password').escape(),
 ], async (req, res) => {
   const checkUserQuery = await User.checkQuery(req);
-  console.log(checkUserQuery);
+  console.log(checkUserQuery.id);
   if (checkUserQuery.id > 0) {
-
-    req.session.username = checkUserQuery.username;
     req.session.loggedin = true;
     req.session.email = checkUserQuery.emails;
     req.session.iq = checkUserQuery.id;
-
-    res.redirect(`/users/${checkUserQuery.id}/userpage`);
-  } else if (checkUserQuery.id === 0) {
+    res.redirect(`/users/database`);
+  } else {
     res.redirect('/login?msg=badPass');
-  } else if (checkUserQuery === 'Error') {
-    res.json({
-      msg: "userDoesNotExist"
-    })
   }
-});
 
-router.get('/:userId/userpage', async (req, res) => {
-  const theId = parseInt(req.params.userId, 10);
-  const userInfo = await User.getAll(theId);
-  res.render('userpage', {
-    title: 'Userpage',
-    username: userInfo.username
-    // locals: {
-    //   title: 'Userpage',
-    //   displayname: userInfo.displayname
-    // }
-  });
 });
 
 module.exports = router;
